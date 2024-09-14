8a1d1a52b6c09c9d0e782a50c928841da99af5ef
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2024-09-14 21:07:33 +0700

    fix: Can not disable batch number

--- MaskNodes.py
+++ MaskNodes.py
@@ -57,6 +57,7 @@ class CustomSaveImage:
                 filename_with_batch_num = filename.replace("%batch_num%", str(batch_number))
                 file = f"{filename_with_batch_num}_{counter:05}_.png"
             else:
+                filename_with_batch_num = filename.replace("%batch_num%", "")
                 file = f"{filename_with_batch_num}.png"
 
             img.save(os.path.join(full_output_folder, file), pnginfo=metadata, compress_level=self.compress_level)
